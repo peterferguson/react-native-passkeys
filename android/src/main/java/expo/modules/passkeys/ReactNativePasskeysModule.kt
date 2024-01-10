16cdfd2e02d4217c77eae364adef57a774dc7602
Andrew <alevy97@gmail.com>
2024-01-10 15:55:31 -0500

    added error handling

--- android/src/main/java/expo/modules/passkeys/ReactNativePasskeysModule.kt
+++ android/src/main/java/expo/modules/passkeys/ReactNativePasskeysModule.kt
@@ -8,6 +8,21 @@ import androidx.credentials.CreatePublicKeyCredentialRequest
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetPublicKeyCredentialOption
+import androidx.credentials.exceptions.CreateCredentialCancellationException
+import androidx.credentials.exceptions.CreateCredentialException
+import androidx.credentials.exceptions.CreateCredentialInterruptedException
+import androidx.credentials.exceptions.CreateCredentialProviderConfigurationException
+import androidx.credentials.exceptions.CreateCredentialUnknownException
+import androidx.credentials.exceptions.CreateCredentialUnsupportedException
+import androidx.credentials.exceptions.GetCredentialCancellationException
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.credentials.exceptions.GetCredentialInterruptedException
+import androidx.credentials.exceptions.GetCredentialProviderConfigurationException
+import androidx.credentials.exceptions.GetCredentialUnknownException
+import androidx.credentials.exceptions.GetCredentialUnsupportedException
+import androidx.credentials.exceptions.NoCredentialException
+import androidx.credentials.exceptions.publickeycredential.CreatePublicKeyCredentialDomException
+import androidx.credentials.exceptions.publickeycredential.GetPublicKeyCredentialDomException
 import com.google.gson.Gson
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
@@ -34,9 +49,11 @@ class ReactNativePasskeysModule : Module() {
         }
 
         AsyncFunction("create") { request: PublicKeyCredentialCreationOptions, promise: Promise ->
-            val credentialManager = CredentialManager.create(appContext.reactContext?.applicationContext!!)
+            val credentialManager =
+                CredentialManager.create(appContext.reactContext?.applicationContext!!)
             val json = Gson().toJson(request)
             val createPublicKeyCredentialRequest = CreatePublicKeyCredentialRequest(json)
+
 
             mainScope.launch {
                 try {
@@ -45,32 +62,101 @@ class ReactNativePasskeysModule : Module() {
                     }
                     val response =
                         result?.data?.getString("androidx.credentials.BUNDLE_KEY_REGISTRATION_RESPONSE_JSON")
-                    val createCredentialResponse = Gson().fromJson(response, RegistrationResponseJSON::class.java)
+                    val createCredentialResponse =
+                        Gson().fromJson(response, RegistrationResponseJSON::class.java)
                     promise.resolve(createCredentialResponse)
-                } catch (e: expo.modules.kotlin.exception.CodedException) {
-                    promise.reject(e)
+                } catch (e: CreateCredentialException) {
+                    promise.reject("Passkey Create", getRegistrationException(e), e)
                 }
             }
         }
 
         AsyncFunction("get") { request: PublicKeyCredentialRequestOptions, promise: Promise ->
-            val credentialManager = CredentialManager.create(appContext.reactContext?.applicationContext!!)
+            val credentialManager =
+                CredentialManager.create(appContext.reactContext?.applicationContext!!)
             val json = Gson().toJson(request)
-            val getCredentialRequest = GetCredentialRequest(listOf(GetPublicKeyCredentialOption(json)))
+            val getCredentialRequest =
+                GetCredentialRequest(listOf(GetPublicKeyCredentialOption(json)))
 
             mainScope.launch {
                 try {
                     val result = appContext.currentActivity?.let {
-                            credentialManager.getCredential(it, getCredentialRequest)
+                        credentialManager.getCredential(it, getCredentialRequest)
                     }
                     val response =
                         result?.credential?.data?.getString("androidx.credentials.BUNDLE_KEY_AUTHENTICATION_RESPONSE_JSON")
-                    val createCredentialResponse = Gson().fromJson(response, AuthenticationResponseJSON::class.java)
+                    val createCredentialResponse =
+                        Gson().fromJson(response, AuthenticationResponseJSON::class.java)
                     promise.resolve(createCredentialResponse)
-                } catch (e: expo.modules.kotlin.exception.CodedException) {
-                    promise.reject(e)
+                } catch (e: GetCredentialException) {
+                    promise.reject("Passkey Get", getAuthenticationException(e), e)
                 }
             }
         }
     }
+
+    private fun getRegistrationException(e: CreateCredentialException) =
+        when (e) {
+            is CreatePublicKeyCredentialDomException -> {
+                e.domError.toString()
+            }
+
+            is CreateCredentialCancellationException -> {
+                "UserCancelled"
+            }
+
+            is CreateCredentialInterruptedException -> {
+                "Interrupted"
+            }
+
+            is CreateCredentialProviderConfigurationException -> {
+                "NotConfigured"
+            }
+
+            is CreateCredentialUnknownException -> {
+                "UnknownError"
+            }
+
+            is CreateCredentialUnsupportedException -> {
+                "NotSupported"
+            }
+
+            else -> e.toString()
+        }
+
+    private fun getAuthenticationException(e: GetCredentialException) =
+        when (e) {
+            is GetPublicKeyCredentialDomException -> {
+                e.domError.toString()
+            }
+
+            is GetCredentialCancellationException -> {
+                "UserCancelled"
+            }
+
+            is GetCredentialInterruptedException -> {
+                "Interrupted"
+            }
+
+            is GetCredentialProviderConfigurationException -> {
+                "NotConfigured"
+            }
+
+            is GetCredentialUnknownException -> {
+                "UnknownError"
+            }
+
+            is GetCredentialUnsupportedException -> {
+                "NotSupported"
+            }
+
+            is NoCredentialException -> {
+                "NoCredentials"
+            }
+
+            else -> {
+                e.toString()
+            }
+        }
+
 }
